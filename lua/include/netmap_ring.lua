86fee1d8c535e63a79b48c3fbb5eaf059b54138d
Dominik Schoeffmann <dominik.schoeffmann@gmail.com>
2016-03-12 10:06:46 +0100

    inital netmap_ring

---
+++ lua/include/netmap_ring.lua
@@ -0,0 +1,54 @@
+local ffi = require "ffi"
+local netmapc = require "netmapc"
+
+local Netmap_ring = {}
+Netmap_ring.__index = Netmap_ring
+
+ffi.metatype("netmap_ring*", Netmap_ring)
+
+-- Returns a netmap ring object
+function Netmap_ring:create(fd, nifp, index, tx)
+	local c
+	if tx == 1 then
+		c = netmapc.NETMAP_TXRING(nifp, index)
+	else if tx == 0 then
+		c = netmapc.NETMAP_RXRING(nifp, index)
+	end
+	else 
+		print("netmap_ring: tx should be 1 or 0")
+	end
+	r = {}
+	r.tx = tx
+	r.fd = fd
+	r.c = c
+	r.pos = c.head
+	c.cur = c.head
+	setmetatable(r, self)
+	return r
+end
+
+-- Synchronizes the user view with the OS/HW view
+function Netmap_ring:sync()
+	self.c.cur = self.pos
+	self.c.head = self.pos
+	if self.tx == 1 then
+		netmapc.ioctl(self.fd, NIOCTXSYNC)
+		return
+	else if self.tx == 0 then
+		netmapc.ioctl(self.fd, NIOCRXSYNC)
+		return
+	end
+	print("Netmap_ring:sync() tx should have been 1 or 0")
+end
+
+-- Retuns how many slots are available in the ring
+function Netmap_ring:avail()
+	return netmapc.nm_ring_space(self.c)
+end
+
+-- Returns the index of the next slot
+function Netmap_ring:next_slot()
+	return netmapc.nm_ring_next(self.c, self.pos);
+end
+
+return Netmap_ring
