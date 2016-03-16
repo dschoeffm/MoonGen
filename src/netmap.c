13fac374a839d8a04b108c9c1ad8e575c653e979
Dominik Schoeffmann <dominik.schoeffmann@gmail.com>
2016-03-16 15:49:59 +0100

    added netmap C support code

---
+++ src/netmap.c
@@ -0,0 +1,47 @@
+#include <net/netmap_user.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <poll.h>
+
+struct netmap_if* NETMAP_IF_wrapper(uint64_t base, uint32_t ofs){
+	return NETMAP_IF(base, ofs);
+}
+
+struct netmap_ring* NETMAP_TXRING_wrapper(struct netmap_if* nifp, uint32_t index){
+	return NETMAP_TXRING(nifp, index);
+}
+
+struct netmap_ring* NETMAP_RXRING_wrapper(struct netmap_if* nifp, uint32_t index){
+	return NETMAP_RXRING(nifp, index);
+}
+
+char* NETMAP_BUF_wrapper(struct netmap_ring* ring, uint32_t index){
+	return NETMAP_BUF(ring, index);
+}
+
+uint64_t NETMAP_BUF_IDX_wrapper(struct netmap_ring* ring, char* buf){
+	return NETMAP_BUF_IDX(ring, buf);
+}
+
+int open_wrapper(){
+	return open("/dev/netmap", O_RDWR);
+}
+
+int ioctl_NIOCGINFO(int fd, struct nmreq* nmr){
+	return ioctl(fd, NIOCGINFO, nmr);
+}
+
+int ioctl_NIOCREGIF(int fd, struct nmreq* nmr){
+	return ioctl(fd, NIOCREGIF, nmr);
+}
+
+int ioctl_NIOCTXSYNC(int fd){
+	return ioctl(fd, NIOCTXSYNC);
+}
+
+int ioctl_NIOCRXSYNC(int fd){
+	return ioctl(fd, NIOCRXSYNC);
+}
