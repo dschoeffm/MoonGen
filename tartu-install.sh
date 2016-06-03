0692e83a6844600106c4d26f26e805263d0ac776
Dominik Schoeffmann <schoeffm@in.tum.de>
2016-06-03 12:54:39 +0200

    delete unused install script

--- tartu-install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-apt install gcc g++ cmake make kernel-headers-3.13-1-grml-amd64 linux-source-3.13
-git clone https://github.com/dschoeffm/MoonGen.git
-git clone https://github.com/dschoeffm/netmap.git
