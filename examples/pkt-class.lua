b4326b6ef2545d58089ed6cde74946955466dd5c
Dominik Schoeffmann <dominik.schoeffmann@gmail.com>
2018-07-28 11:57:45 +0200

    change libmoon to pkt-class + fix example script

--- examples/pkt-class.lua
+++ examples/pkt-class.lua
@@ -14,7 +14,6 @@ function configure(parser)
 	parser:argument("dev2", "Device to use for classification."):args(1):convert(tonumber)
 	parser:argument("file", "File to replay."):args(1)
 	parser:argument("algo", "Classification algorithm."):args(1)
---	parser:option("-r --rate-multiplier", "Speed up or slow down replay, 1 = use intervals from file, default = replay as fast as possible"):default(0):convert(tonumber):target("rateMultiplier")
 	parser:flag("-l --loop", "Repeat pcap file.")
 	local args = parser:parse()
 	return args
@@ -24,10 +23,6 @@ function master(args)
 	local devSend = device.config{port = args.dev1}
 	local devRecv = device.config{port = args.dev2}
 	device.waitForLinks()
---	local rateLimiter
---	if args.rateMultiplier > 0 then
---		rateLimiter = limiter:new(dev:getTxQueue(0), "custom")
---	end
 	mg.startTask("replay", dev1:getTxQueue(0), args.file, args.loop)
 	mg.startTask("pktClassSlave", dev2:getRxQueue(0), args.algo)
 
@@ -43,50 +38,26 @@ function replay(queue, file, loop, rateLimiter, multiplier)
 	local linkSpeed = queue.dev:getLinkStatus().speed
 	while mg.running() do
 		local n = pcapFile:read(bufs)
-		if n > 0 then
-			if rateLimiter ~= nil then
-				if prev == 0 then
-					prev = bufs.array[0].udata64
-				end
-				for i, buf in ipairs(bufs) do
-					-- ts is in microseconds
-					local ts = buf.udata64
-					if prev > ts then
-						ts = prev
-					end
-					local delay = ts - prev
-					delay = tonumber(delay * 10^3) / multiplier -- nanoseconds
-					delay = delay / (8000 / linkSpeed) -- delay in bytes
-					buf:setDelay(delay)
-					prev = ts
-				end
-			end
-		else
+		if n <= 0 then
 			if loop then
 				pcapFile:reset()
-			else
-				break
 			end
 		end
-		if rateLimiter then
-			rateLimiter:sendN(bufs, n)
-		else
-			queue:sendN(bufs, n)
-		end
+		queue:sendN(bufs, n)
 	end
 end
 
 function pktClassSlave(queue, algo)
 	local bufs = memory.bufArray()
 	local pktCtr = stats:newPktRxCounter("Packets counted", "plain")
-	local classifier = pktClass.algo
+	local classifier = pktClass[algo]
 	while mg.running() do
 		local rx = queue:tryRecv(bufs, 63)
 		for i = 1, rx do
 			local buf = bufs[i]
 			pktCtr:countPacket(buf)
 		end
-		classifier:run(bufs.array, rx)
+		classifier(bufs.array, rx)
 		bufs:free(rx)
 		pktCtr:update()
 	end
